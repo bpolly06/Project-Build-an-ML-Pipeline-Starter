2639e996c6fcbd1af5789d4db26db6e35ea26bed
bpolly06 <bpolly88@gmail.com>
2025-09-16 11:27:36 -0400

    updated main.py with data_check step, successful run

--- main.py
+++ main.py
@@ -68,9 +68,17 @@ def go(config: DictConfig):
             pass
 
         if "data_check" in active_steps:
-            ##################
-            # Implement here #
-            ##################
+            _ = mlflow.run(
+                os.path.abspath(os.path.join(root_path, "src/data_check")),
+                "main",
+                parameters={
+                    "csv": "clean_sample.csv:latest",
+                    "ref": "clean_sample.csv:reference",
+                    "kl_threshold": config["data_check"]["kl_threshold"],
+                    "min_price": config["etl"]["min_price"],
+                    "max_price": config["etl"]["max_price"]              
+                }
+            )
             pass
 
         if "data_split" in active_steps:
